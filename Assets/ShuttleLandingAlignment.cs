31c22e595bf5ca033bcc1baeb804bd6d90181965
Jacob Crawford <jacobcrawford@hotmail.com>
2020-02-09 15:52:00 +0100

    init

---
+++ Assets/ShuttleLandingAlignment.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShuttleLandingAlignment : MonoBehaviour
+{
+    private GameObject quad;
+    private GameObject shuttle;
+    private GameObject landingLane;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        shuttle = GameObject.Find("ShuttleTarget");
+        quad = GameObject.Find("ShuttleTarget/Quad");
+        landingLane = GameObject.Find("LandingLaneTarget");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float right_o = Vector3.Dot(shuttle.transform.right, landingLane.transform.right);
+        float up_o = Vector3.Dot(shuttle.transform.up, landingLane.transform.up);
+        float forward_o = Vector3.Dot(shuttle.transform.forward, landingLane.transform.forward);
+        float align = right_o + up_o + forward_o;
+
+        if (align <= 3 && align > 2.9)
+        {
+            quad.GetComponent<Renderer>().material.color = new Color(0, 255, 0);
+        }
+        else if (align <= 2.9 && align > 2)
+        {
+            quad.GetComponent<Renderer>().material.color = new Color(125, 125, 0);
+        }
+        else {
+            quad.GetComponent<Renderer>().material.color = new Color(255, 0, 0);
+        }
+
+    }
+}
---
+++ Assets/TransformNoseCoordinates.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformNoseCoordinates : MonoBehaviour
+{
+    private GameObject nose;
+    private GameObject shuttle;
+    private GameObject earth;
+    private Vector3 localEarthPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nose = GameObject.Find("ShuttleTarget/Nose");
+        shuttle = GameObject.Find("ShuttleTarget");
+        earth = GameObject.Find("EarthTarget");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ 
+        // 2 Get Earth matrix
+        Matrix4x4 earthMatrix = earth.transform.worldToLocalMatrix;
+
+        // 3 Get Shuttle matrix
+        Matrix4x4 shuttleMatrix = shuttle.transform.localToWorldMatrix;
+
+        // 4 Transform nose coordinates
+        localEarthPos = shuttleMatrix.MultiplyPoint3x4(nose.transform.localPosition);
+        Debug.Log(nose.transform.localPosition);
+        //localEarthPos = earthMatrix.MultiplyPoint3x4(localEarthPos);
+
+
+
+    }
+
+    private void OnGUI()
+    {
+        GUI.color = Color.red;
+        GUI.Label(new Rect(10, 10, 500, 100), localEarthPos.ToString() );
+    }
+
+
+
+    /**************************************************************************/
+    /************ CONVENIENCE FUNCTIONS FOR AFFINE TRANSFORMATIONS ************/
+    /**************************************************************************/
+
+    public static Matrix4x4 T(float x, float y, float z)
+    {
+        Matrix4x4 m = new Matrix4x4();
+
+        m.SetRow(0, new Vector4(1, 0, 0, x));
+        m.SetRow(1, new Vector4(0, 1, 0, y));
+        m.SetRow(2, new Vector4(0, 0, 1, z));
+        m.SetRow(3, new Vector4(0, 0, 0, 1));
+
+        return m;
+    }
+
+    public static Matrix4x4 Rx(float a)
+    {
+        Matrix4x4 m = new Matrix4x4();
+
+        m.SetRow(0, new Vector4(1, 0, 0, 0));
+        m.SetRow(1, new Vector4(0, Mathf.Cos(a), -Mathf.Sin(a), 0));
+        m.SetRow(2, new Vector4(0, Mathf.Sin(a), Mathf.Cos(a), 0));
+        m.SetRow(3, new Vector4(0, 0, 0, 1));
+
+        return m;
+    }
+
+    public static Matrix4x4 Ry(float a)
+    {
+        Matrix4x4 m = new Matrix4x4();
+
+        m.SetRow(0, new Vector4(Mathf.Cos(a), 0, Mathf.Sin(a), 0));
+        m.SetRow(1, new Vector4(0, 1, 0, 0));
+        m.SetRow(2, new Vector4(-Mathf.Sin(a), 0, Mathf.Cos(a), 0));
+        m.SetRow(3, new Vector4(0, 0, 0, 1));
+
+        return m;
+    }
+
+    public static Matrix4x4 Rz(float a)
+    {
+        Matrix4x4 m = new Matrix4x4();
+
+        m.SetRow(0, new Vector4(Mathf.Cos(a), -Mathf.Sin(a), 0, 0));
+        m.SetRow(1, new Vector4(Mathf.Sin(a), Mathf.Cos(a), 0, 0));
+        m.SetRow(2, new Vector4(0, 0, 1, 0));
+        m.SetRow(3, new Vector4(0, 0, 0, 1));
+
+        return m;
+    }
+
+
+    public static Matrix4x4 S(float sx, float sy, float sz)
+    {
+        Matrix4x4 m = new Matrix4x4();
+
+        m.SetRow(0, new Vector4(sx, 0, 0, 0));
+        m.SetRow(1, new Vector4(0, sy, 0, 0));
+        m.SetRow(2, new Vector4(0, 0, sz, 0));
+        m.SetRow(3, new Vector4(0, 0, 0, 1));
+
+        return m;
+    }
+}
